5ee1cdabce70baea57b9adf083fa24efd88de8e5
Samuel Santos Gomes de Souza <Samuels.g.desouza@gmail.com>
2024-01-03 12:39:22 -0300

    generating ambient variable

--- settings/settings.py
+++ settings/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q4sy-l1ih2$d#mqpvoc__sgmi%i$lw*rh&)a==6lt)d@-1ff_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG") == "True"
+print(DEBUG)
 
 ALLOWED_HOSTS = ["144.126.222.126", "http://144.126.222.126", "127.0.0.1"]
 
